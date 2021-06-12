97f82a86a73af3f8bed7b7be93d94c3f4379a848
Alex Kernan <akernan2@csvlab-vm13.uwg.westga.edu>
2021-06-12 18:59:38 -0400

    HumanPlayer Tests complete

--- src/edu/westga/cs6910/pig/testing/humanPlayer/TestHumanPlayerWhenTakeTurn.java
+++ src/edu/westga/cs6910/pig/testing/humanPlayer/TestHumanPlayerWhenTakeTurn.java
@@ -7,7 +7,12 @@ import org.junit.jupiter.api.Test;
 import edu.westga.cs6910.pig.model.HumanPlayer;
 
 /**
- * 
+ * JUnit Tests that ensures a Human Players turn is taken properly. 
+ * Conditions verified herein are:
+ * 1) Turn ends when player rolls a 1
+ * 2) TurnTotal is zeroized when rolling a 1
+ * 3) when roll !=1, sum of dice are added to turn total.
+ * 4) Haman's Game total remains after rolling a 1 
  * 
  * @author Alex Kernan
  * @version 6/12/2021
@@ -57,19 +62,20 @@ class TestHumanPlayerWhenTakeTurn {
 		testHumanPlayer.setIsMyTurn(true);
 
 		for (int rollCount = 0; rollCount < 100; rollCount++) {
+			testHumanPlayer.setTotal(0);
+			testHumanPlayer.setTurnTotal(0);
 			testHumanPlayer.takeTurn();
 			oneHasBeenRolled = testHumanPlayer.getDicePair().getDie1Value() == 1
 					|| testHumanPlayer.getDicePair().getDie2Value() == 1;
 
 			turnTotal = testHumanPlayer.getTurnTotal();
 			die1Value = testHumanPlayer.getDicePair().getDie1Value();
-			die2Value = testHumanPlayer.getDicePair().getDie1Value();
+			die2Value = testHumanPlayer.getDicePair().getDie2Value();
 
 			if (!oneHasBeenRolled && (turnTotal != (die1Value + die2Value))) {
 				fail("The dice were not added to the turnTotal correctly ");
 			}
-			testHumanPlayer.setTotal(0);
-			testHumanPlayer.setTurnTotal(0);
+			
 		}
 	}
 
@@ -90,4 +96,5 @@ class TestHumanPlayerWhenTakeTurn {
 			}
 		}
 	}
+	
 }
