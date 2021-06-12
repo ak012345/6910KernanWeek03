66444b49a1614eb286facae9e6d3a39c0bd735f2
Alex Kernan <akernan2@csvlab-vm13.uwg.westga.edu>
2021-06-12 19:00:23 -0400

    Update HUmanPLayer test

--- src/edu/westga/cs6910/pig/testing/humanPlayer/TestHumanPlayerWhenResetTurnTotal.java
+++ src/edu/westga/cs6910/pig/testing/humanPlayer/TestHumanPlayerWhenResetTurnTotal.java
@@ -1,11 +1,19 @@
 package edu.westga.cs6910.pig.testing.humanPlayer;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 import edu.westga.cs6910.pig.model.HumanPlayer;
 
+/**
+ * JUnit test that checks if the player's turnTotoal is set to zero via the
+ * resetTurnTotal() method
+ * 
+ * @author Alex Kernan
+ * @version 6/12/2021
+ *
+ */
 class TestHumanPlayerWhenResetTurnTotal {
 
 	@Test
